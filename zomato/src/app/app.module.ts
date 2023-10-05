2fa0b2d0cbb55b3a3381cb1fd5d918917435d1a7
Ajay Kuriakose <ajaykuriakose001@gmail,com>
2023-10-05 10:04:01 +0530

    Latest Chnages

--- zomato/src/app/app.module.ts
+++ zomato/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestarantComponent } from './pages/restarant/restarant.component';
+import { LoginComponent } from './pages/login/login.component'; 
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestarantComponent,
+    LoginComponent,
+    CategoriesComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
